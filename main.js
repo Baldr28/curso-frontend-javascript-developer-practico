fd74dc4d2e396c1b532783be532243631ad970f0
Joaquin <Baldrweb@gmail.com>
2023-11-07 19:09:52 -0300

    animation and responsive menu dropdown added

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 const dropdownFirst = document.getElementById('dropdownFirst');
 const emailbtn = document.getElementById('email');
+const btnMobile = document.getElementById('hamburguer')
+const menuResponsive = document.getElementById('menuResponsive');
 
 emailbtn.addEventListener('click', showDropdown);
 
 function showDropdown(){
     dropdownFirst.classList.toggle('inactive')
+}
+
+menuResponsive.classList.remove('activo');
+
+btnMobile.addEventListener('click', displayMenu);
+
+function displayMenu(){
+    menuResponsive.classList.toggle('display')
+    menuResponsive.classList.toggle('oculto')
 }
